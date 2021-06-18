e5eb3471e295eb1a8e8fdbd4f73e75fadebd54ac
Vinitha3 <84272848+Vinitha3@users.noreply.github.com>
2021-06-18 11:29:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const card = document.querySelector('.card')
+const container = document.querySelector('.card-wrapper')
+const title = document.querySelector('.img-1')
+
+console.log(window.pageXOffset)
+
+
+container.addEventListener('mousemove', (e) => {
+ console.log(e.pageY);
+ let xAxis = (window.innerWidth/2 - e.pageX)/10;
+ let yAxis = (window.innerHeight/2 - e.pageY)/10;
+ card.style.transform = (`rotateY(${yAxis}deg) rotateX(${xAxis}deg)`);
+ title.style.transform = `translateZ(150px) rotateZ(-40deg)`
+});
+
+container.addEventListener('mouseenter',(e => {
+ card.style.transform = "none";
+ title.style.transform = `none`;
+}))
+
+
+container.addEventListener('mouseleave',(e) => {
+ card.style.transition = "all 0.5s ease"
+ title.style.transition =`all 0.5s ease`
+ card.style.transform = (`rotateY(0deg) rotateX(0deg)`);
+ title.style.transform = `translateZ(0px) rotateZ(0)`
+})
